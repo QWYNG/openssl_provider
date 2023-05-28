42a556b223b0f3665dba5089a73e6b125c23cb0a
qwyng <ikusawasi@gmail.com>
2023-05-28 16:11:28 +0900

    rm unnecessary file

--- lib/openssl_provider.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "openssl_provider/version"
-
-module OpensslProvider
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/openssl_provider/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module OpensslProvider
-  VERSION = "0.1.0"
-end
